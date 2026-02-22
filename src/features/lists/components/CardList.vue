58f00a08886f9744b80f5aac5945c17e3c621358
MertVural <mertvural@scorpapp.com>
2026-02-22 11:54:26 +0300

    style: for mobile gap changed

--- src/features/lists/components/CardList.vue
+++ src/features/lists/components/CardList.vue
@@ -20,7 +20,7 @@ const { lists } = storeToRefs(useListingStore());
   </div>
 
   <section
-    class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5"
+    class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 md:gap-5"
   >
     <Card v-for="item in lists" :key="item.id" :item="item" />
   </section>
